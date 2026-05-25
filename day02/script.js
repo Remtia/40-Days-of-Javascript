284e3baf0b93b3496a167fd5fba11d22e159a007
Remtia <224189853+Remtia@users.noreply.github.com>
2026-05-25 12:57:47 +0000

    Assignment done

---
+++ day02/script.js
@@ -0,0 +1,26 @@
+let name = "Remtia";
+let age = 23;
+let isStudent = true;
+let favoriteProgrammingLang = "Javascript"
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favoriteProgrammingLang);
+
+let anObject = {};
+let anArray = [];
+
+anObject.name = name;
+anObject.age = age;
+anObject.status = isStudent;
+
+anArray.push(0)
+anArray.push(1)
+anArray.push(2)
+
+console.log(anObject);
+console.log(anArray);
+
+
+
